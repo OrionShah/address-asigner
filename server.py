6dfcf2b5ca29de5989c9eae5de7f52784a68efa4
Andrey Shahov <andrey.shahov@webpp.ru>
2019-12-03 22:31:02 +0400

    Настроено корректное распределение

--- server.py
+++ server.py
@@ -1,10 +1,7 @@
-import json
-import os
 import pickle
 import random
-from math import ceil
 
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, render_template
 from redis import Redis
 
 app = Flask(__name__)
@@ -27,41 +24,56 @@ def uik(uikkey):
     uik = pickle.loads(redis.get(uikkey))
     houses = []
     i = 1
-    mid = ceil(uik['people'] / uik['places'])
     generated = 0
-    all_people = uik['people']
-    for addr,data in uik['houses'].items():
-        people, all_people = generate_people(data['place'], mid, all_people)
-        # print(people, all_people)
-        generated += people
+    addrs = generate_people(uik['people'], uik['places'])
+    last = 0
+    for addr, data in uik['houses'].items():
         place = ', '.join(data['place']) if len(data['place']) else '-'
         coords = ','.join(data['coords'])
+
+        places = len(data['place']) if len(data['place']) else 1
+        people = sum([v for k,v in addrs.items() if last <= k < last+places])
+        last += places
+        generated += people
+
         data.update({'address': ', '.join(addr), 'place': place,
                      'coords': coords, 'i': i, 'people': people})
         houses.append(data)
         i += 1
     uik['houses'] = houses
     uik['generated'] = generated
-    # return jsonify(uik)
     return render_template('uik.html', uik=uik, uikkey=uikkey)
 
 
-def generate_people(places, mid, all_people):
-    if len(places):
-        peoples = 0
-        for place in places:
-            inc_peoples, all_people1 = generate_people([], mid, all_people)
-            peoples += inc_peoples
-    else:
-        iter = range(1, mid)
-        weights = [.2*(2 if i%2 == 0 else 3) for i in range(len(iter))]
-        peoples = random.choices(iter, weights=weights)[0]
-        # import pdb
-        # pdb.set_trace()
-    if peoples > all_people:
-        peoples = all_people
-    # print(peoples, all_people)
-    all_people -= peoples
-    # print(all_people)
-    # print('------')
-    return peoples, all_people
+def generate_people(peoples: int, places: int):
+    if places < 1:
+        return {i: 0 for i in range(places)}
+    mid = int(round(peoples/places, 1))
+    addrs = {i: mid for i in range(places)}
+    buff = peoples - (places * mid)
+    i = 0
+    max_people = int(mid * 1.5)
+    while buff:
+        if max_people > addrs[i] > 4:
+            diff = random.randint(0, max_people-addrs[i])
+            if diff > buff:
+                diff = buff
+            buff -= diff
+            addrs[i] += diff
+        if addrs[i] > max_people:
+            diff = addrs[i] - random.randint(max_people-2, max_people+4)
+            if diff > buff:
+                diff = buff
+            buff += diff
+            addrs[i] -= diff
+        if addrs[i] < 2:
+            diff = random.randint(0, mid+1)
+            if diff > buff:
+                diff = buff
+            buff -= diff
+            addrs[i] += diff
+
+        i += 1
+        if i > places-1:
+            i = 0
+    return addrs
