a17652d181f7ded301670b0a2a4f2b553befce46
Andrey Shahov <andrey.shahov@webpp.ru>
2019-12-14 16:38:04 +0400

    Смена типа ответа

--- server.py
+++ server.py
@@ -87,7 +87,7 @@ def support_jsonp(f):
         callback = request.args.get('callback', False)
         if callback:
             content = (str(callback) + '(' + str(f()) + ')').replace('\'', '\"')
-            return app.response_class(content, mimetype='application/text')
+            return app.response_class(content, mimetype='application/jsonp')
         else:
             return f(*args, **kwargs)
     return decorated_function
